0ff8fcaf4d4da081f9fc1f1aed46e5a32b4be367
jalho <alho.jere@gmail.com>
2021-09-19 15:08:23 +0300

    Initial commit

---
+++ cpToMyGames.sh
@@ -0,0 +1,3 @@
+cp label.xml /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/test_eba62d5.xml
+cp script.xs /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/test_eba62d5.xs
+cp "rmx 5-0-0.xs" /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/
