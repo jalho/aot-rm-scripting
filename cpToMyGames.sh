c1a750e0f8f54bab5768cb0b4cced5710cd01a5b
jalho <alho.jere@gmail.com>
2021-09-21 17:24:17 +0300

    Update descriptions

--- cpToMyGames.sh
+++ cpToMyGames.sh
@@ -1,3 +1,3 @@
-cp label.xml /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/test_eba62d5.xml
-cp script.xs /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/test_eba62d5.xs
+cp label.xml /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/56972bc2353e1295c2b00c6f18526aabcd6290a4.xml
+cp script.xs /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/56972bc2353e1295c2b00c6f18526aabcd6290a4.xs
 cp "rmx 5-0-0.xs" /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/
