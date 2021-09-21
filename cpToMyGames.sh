7627900bce3eb24ad8d422c5fb354e11f00a222c
jalho <alho.jere@gmail.com>
2021-09-21 18:44:26 +0300

    Setup build scripts with hashing

--- cpToMyGames.sh
+++ cpToMyGames.sh
@@ -1,3 +1,9 @@
-cp label.xml /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/56972bc2353e1295c2b00c6f18526aabcd6290a4.xml
-cp script.xs /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/56972bc2353e1295c2b00c6f18526aabcd6290a4.xs
-cp "rmx 5-0-0.xs" /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/
+BUILD_ID=$(sha1sum script.xs | awk '{print $1}')
+SED_ARG="s/BUILDID/$BUILD_ID/"
+mkdir -p build
+cp script.xs build/script.xs
+cp label.xml build/label.xml
+sed -i $SED_ARG build/script.xs
+sed -i $SED_ARG build/label.xml
+cp build/script.xs /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/$BUILD_ID.xs
+cp build/label.xml /mnt/c/Users/alhoj/Documents/"My Games"/"Age of Mythology"/RM2/$BUILD_ID.xml
